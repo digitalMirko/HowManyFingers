6f1d0abb705dc65244c23aa68066534d16541b46
Mirko Cukich <mirkocukich@gmail.com>
2018-01-15 14:39:08 -0600

    Storyboard connected to ViewController

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var guessTextField: UITextField!
+    
+    @IBOutlet weak var answerLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func guessBtn(_ sender: Any) {
+        
+    }
+    
+    
 
 }
 
